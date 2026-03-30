cfb7cf38e73d17eed86a297c9a104ec0b56f40c8
Henjo Hoeksma <henjo@upassist.com>
2026-03-30 17:15:43 +0200

    fix: use getProperties() instead of hasProperty() for Neos 8.3 compat
    NodeType::hasProperty() does not exist in Neos 8.3.
    Use array_key_exists on getProperties() instead.

--- Classes/Service/ReviewStatusService.php
+++ Classes/Service/ReviewStatusService.php
@@ -29,7 +29,7 @@ class ReviewStatusService
             return;
         }
 
-        if (!$documentNode->getNodeType()->hasProperty('reviewStatus')) {
+        if (!array_key_exists('reviewStatus', $documentNode->getNodeType()->getProperties())) {
             return;
         }
 
